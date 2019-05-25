6e3f870151d1a061d2fb556f7ee7cc57ea198c31
Daniel Sink <17501944+sinkda@users.noreply.github.com>
2019-05-25 15:36:51 -0400

    Initial commit of awow addon .

---
+++ DayZero.lua
@@ -0,0 +1,113 @@
+-- Config Store
+DZData = {};
+
+-- Default Values
+if DZData.FirstLogin == nil then
+
+    DZData.FirstLogin = true;
+    DZData.NameplateDistance = "100";
+    DZData.NameplateColor = 1;
+
+end
+
+
+-- Helper Functions
+local function SetNameplateDistance(val)
+    if not tonumber(val) then
+        return print("Error setting nameplate distance -- non number provided.");
+    end
+
+    DZData.NameplateDistance = val;
+    print("Nameplate Distance set to: ".. val);
+    SetCVar("nameplateMaxDistance", DZData.NameplateDistance);
+end
+
+local function SetNameplateClassColor(val)
+    if not tonumber(val) then
+        return print("Error setting class color -- 0 or 1 not provided.");
+    end
+
+    if(val == 1) or val == "1" then
+        print("|cFF0066FFClass Color Enabled");
+        DZData.NameplateColor = 1;
+    else    
+        print("Class Color Disabled");
+        DZData.NameplateColor = 0;
+    end
+    
+    SetCVar("ShowClassColorInFriendlyNameplate", DZData.NameplateColor);
+    SetCVar("ShowClassColorInNameplate", DZData.NameplateColor);
+    
+end
+
+-- Register Slash Commands
+SLASH_DAYZERO1 = "/dz";
+SLASH_DAYZERO2 = "/dayzero";
+SlashCmdList["DAYZERO"] = function(input)
+
+    -- split the input string
+    -- format should be i.e.: /dz distance 80
+
+    local cmd = {};
+    local pattern = string.format("([^%s]+)", " ");
+    string.gsub(input, pattern, function(c) cmd[#cmd + 1] = c end);
+
+
+    local command = "";
+    local value = "";
+    for k,v in pairs(cmd) do
+        if k == 1 then
+            command = v;
+        elseif k == 2 then
+            value = v;
+        else
+            return print("Invalid command provided");
+        end
+    end
+
+    -- check the command and pass the value
+    if command == "distance" then
+        SetNameplateDistance(value);
+    elseif command == "color" then
+        SetNameplateClassColor(value);
+    elseif command == "help" then
+        print("DayZero Help:");
+        print("/dz distance <value>");
+        print("/dz color <value> (0=disable or 1=enable)");
+    end
+
+end
+
+-- Event Register
+local dz = CreateFrame("Frame");
+dz:RegisterEvent("PLAYER_LOGIN");
+dz:RegisterEvent("ADDON_LOADED");
+
+dz:SetScript("OnEvent", function(self, event)
+
+    -- Get action bars to load
+    if event == "ADDON_LOADED" then
+        if DZData.FirstLogin then 
+            SetActionBarToggles(1,1,1,1); -- show all the action bars
+            return;
+        end
+    end
+
+    -- Welcome to game
+    if DZData.FirstLogin then
+        local name = GetUnitName("player");
+        print("Welcome to WoW Classic ".. name .."! Best of Luck out there!");
+        DZData.FirstLogin = false;
+    end
+
+    -- Run settings on player login
+    SetNameplateClassColor(DZData.NameplateColor);
+    SetNameplateDistance(DZData.NameplateDistance);
+
+    -- Some non-configurable C-Vars that are easy to change in game
+    SetCVar("nameplateShowEnemies", 1); -- show enemy nameplates by default
+    
+
+
+    self:UnregisterEvent("PLAYER_LOGIN");
+end);
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# DayZero
+# DayZero Addon for WoW Classic
+
+This addon sets up a new character in WoW Classic for their first adventure! Many of the options in here are CVars and as such are not easily altered.
+
+## Addon Options
+
+You can control the addon values as well using the following:
+```
+/dz distance <value>
+/dz color <value>
+```
+
+The distance value is the draw distance for nameplates. This goes from 0 - 100.
+The color value is friendly and enemy nameplates to have class colors.  0 = disable, 1 = enable.
+
+## Action Bar
+
+I also added on enabling the 4 extra action bars.  This is not currently toggleable via the addon as it can be updated from the interface options window.  This code will only run once for each character you create, but can be removed in the DayZero.lua file.  Locate the following code and remove it:
+
+```lua
+-- Get action bars to load
+    if event == "ADDON_LOADED" then
+        if DZData.FirstLogin then 
+            SetActionBarToggles(1,1,1,1); -- show all the action bars
+            return;
+        end
+    end
+```
